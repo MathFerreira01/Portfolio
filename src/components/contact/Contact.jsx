45181f9324b18316c80b90bb522930401f7925a2
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-05-27 20:37:27 -0300

    feito o HTML e estilização do component Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import './contact.css'
+
+import {MdOutlineMail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+
 function Contact() {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+          <div className="contact_options">
+            <article className='contact_option'>
+              <MdOutlineMail className='contact_option-icon'/>
+              <h4>Email</h4>
+              <h5>ferreiramatheus124@gmail.com</h5>
+              <a href='ferreiramatheus124@gmail.com'>Send a message</a>
+            </article>
+            <article className='contact_option'>
+              <BsWhatsapp className='contact_option-icon'/>
+              <h4>WhatsApp</h4>
+              <h5>+55 61 996194498</h5>
+              <a href='https://api.whatsapp.com/send?phone+5561996194498'>Send a message</a>
+            </article>
+          </div>
+          {/* END OF CONTACT OPTIONS */}
+          <form action=''>
+            <input type="text" name='name' placeholder='Your Full Name' required/>
+            <input type="email" name="email" placeholder="Email"/>
+            <textarea name='message' rows="7" placeholder="Your Message" required></textarea>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+      </div>
+    </section>
   )
 }
 
