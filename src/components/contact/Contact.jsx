663a25cc6d35e059d82d737ce53278658e3b40bb
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-05-29 21:39:03 -0300

    Adicionado o Emailjs e a função para receber os dados do input no email pessoal

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 import './contact.css'
 
 import {MdOutlineMail} from 'react-icons/md'
 import {BsWhatsapp} from 'react-icons/bs'
 
+
 function Contact() {
+
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mhnjpyq', 'template_wck0lgd', form.current, 'rCVWvSboMo5saRwEM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -27,7 +43,7 @@ function Contact() {
             </article>
           </div>
           {/* END OF CONTACT OPTIONS */}
-          <form action=''>
+          <form ref={form} onSubmit={sendEmail}>
             <input type="text" name='name' placeholder='Your Full Name' required/>
             <input type="email" name="email" placeholder="Email"/>
             <textarea name='message' rows="7" placeholder="Your Message" required></textarea>
