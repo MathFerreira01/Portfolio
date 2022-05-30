24ca528205f4d9ca3ed2ddb504e5075bc8b3ba7b
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-05-29 21:56:32 -0300

    Concluído o html e estilização do component footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import "./footer.css"
+
+import {FiInstagram} from 'react-icons/fi'
+import {RiTwitterFill} from 'react-icons/ri'
+
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <a href='#' className='footer_logo'>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Experience</a></li>
+        <li><a href='#'>Portfolio</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href='https://instagram.com'><FiInstagram/></a>
+        <a href='https://twitter.com'><RiTwitterFill/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
