ce370e96a2d4090639ebb905923d7294fb843bca
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-05-31 19:35:28 -0300

    refeito o Height da iamgem dos portfolios

--- src/pages/portfolio/dataPortfolio.jsx
+++ src/pages/portfolio/dataPortfolio.jsx
@@ -1,44 +1,54 @@
 import React from "react";
-import Project from "../../assets/project1.jpg";
-import Project2 from "../../assets/Project2.jpg";
-import Project3 from "../../assets/project3.jpg";
+import Twitter from "../../assets/Twitter.jpeg";
+import Netflix from "../../assets/netflix.jpeg";
+import YouTube from "../../assets/Youtube.jpeg";
+import WhatsApp from "../../assets/whatsapp.jpeg"; 
+import SmartBank from "../../assets/SmartBank.jpeg"; 
+import Discord from "../../assets/Discord.jpeg"; 
 
 const data = [
   {
     id: 1,
-    image: Project,
+    image: Twitter,
     title: "Clone do Twitter",
     github: "https://github.com/MathFerreira01/Clone-Twitter",
     demo: "https://mathferreira01.github.io/Clone-Twitter/",
   },
   {
     id: 2,
-    image: Project2,
+    image: Netflix,
     title: "Clone do Netflix",
     github: "https://github.com/MathFerreira01/clone_netflix",
     demo: "https://mathferreira01.github.io/clone_netflix/",
   },
   {
     id: 3,
-    image: Project,
+    image: YouTube,
     title: "Clone do YouTube",
     github: "https://github.com/MathFerreira01/clone-youtube",
     demo: "https://mathferreira01.github.io/clone-youtube/",
   },
   {
     id: 4,
-    image: Project3,
+    image: WhatsApp,
     title: "Clone do Whatsapp",
     github: "https://github.com/MathFerreira01/clone-youtube",
     demo: "https://mathferreira01.github.io/clone_whatsapp/",
   },
   {
     id: 5,
-    image: Project,
+    image: SmartBank,
     title: "Smart Bank",
     github: "https://github.com/MathFerreira01/clone_whatsapp",
     demo: "https://mathferreira01.github.io/Conta-bancaria/",
   },
+  {
+    id: 6,
+    image: Discord,
+    title: "Interface do Discord",
+    github: "https://github.com/MathFerreira01/Interface-do-Discord",
+    demo: "https://mathferreira01.github.io/Interface-do-Discord/",
+  }
 ];
 
 function DataPortfolio() {
